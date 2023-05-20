84a87c7870651beaf75d9847829e25120051c6c2
PetrenkoS <petrenko.s@yahoo.com>
2023-05-19 22:39:36 -0400

    Refactoring and new profile test

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,12 +1,68 @@
+import com.github.javafaker.Faker;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.time.Duration;
+import java.util.Locale;
+
 public class BaseTest {
+    static WebDriver driver;
+
 
     @BeforeSuite
-    static void setupClass() {
+    static void setupDriver() {
         WebDriverManager.chromedriver().setup();
     }
 
+    @BeforeMethod
+    public void setUpBrowser(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown(){
+        driver.quit();
+    }
+
+    public String generateRandomName(){
+        Faker faker = new Faker(new Locale("en-US"));
+        String newName = faker.name().firstName();
+        return newName;
+    }
+
+    protected void clickLoginBtn() {
+        WebElement submitLogin = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitLogin.click();
+    }
+
+    public void enterPassword(String password) {
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+        passwordInput.click();
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
+    }
+
+    protected void enterEmail(String email) {
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        emailInput.click();
+        emailInput.clear();
+        emailInput.sendKeys(email);
+    }
+
+    public void openUrl() {
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+    }
 }
