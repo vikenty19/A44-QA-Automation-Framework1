97a395960a78fa760ee31a390288ca5b125abe35
vikenty19 <vikenty.plakhov@testpro.io>
2023-05-24 18:53:58 -0400

    Homework18

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,18 +1,34 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.util.List;
 
 public class Homework18 extends BaseTest {
 
     @Test
-    public void playSong() {
-       // String text = "Dark Days";
-       // String playlistName = generateRandomPlaylistName();
+    public void playSong() throws InterruptedException{
+        // String playlistName = generateRandomPlaylistName();
         // login
         openUrl();
         login("vicplach123@gmail.com", "MEGAdelta06@");
+
+        // all songs
+        WebElement allSongs = driver.findElement(By.cssSelector(".songs"));
+        allSongs.click();
+
+
+        // Play the song
+       WebElement hoverable = driver.findElement(By.cssSelector("span[role='button'] > .fa.fa-play"));
+        new Actions(driver)
+                .moveToElement(hoverable)
+                .perform();
+        Thread.sleep(5000);
+       /* WebElement clickable = driver.findElement(By.id("clickable"));
+        new Actions(driver)
+                .doubleClick(clickable)
+                .perform();*/
+
+       // WebElement playBtn = driver.findElement(By.cssSelector("span[title='Play or resume']"));
+        //playBtn.click();
+    }
+}
