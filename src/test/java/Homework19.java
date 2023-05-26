5b26527e04ef25c441bf32a8e1e443e3bcca1f20
vikenty19 <vikenty.plakhov@testpro.io>
2023-05-26 11:14:21 -0400

    homework19

---
+++ src/test/java/Homework19.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Homework19 extends BaseTest {
+
+    @Test
+    public void deletePlaylist() {
