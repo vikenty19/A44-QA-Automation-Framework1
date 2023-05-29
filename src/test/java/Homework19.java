ce25abb350ea974d74dc8c0f031d7b8023434aed
vikenty19 <vikenty.plakhov@testpro.io>
2023-05-29 16:37:25 -0400

    Update Homework19.java

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -15,45 +15,67 @@ public class Homework19 extends BaseTest {
         String playlistName = generateRandomPlaylistName();
         String text = "Dark Days";
         // search for song
+        inputSearch(text);
+
+
+
+        // click results view all button
+        clickViewAllBtn();
+
+        // click first song
+        clickFirstSearchResultSong();
+
+        // click Add To
+        clickAddToPlaylistBtn();
+
+        // create new playlist
+        createNewPlaylistWhileAddingSong(playlistName);
+
+
+        isBannerDisplayed();
+
+        // check the song
+        getSongName();
+
+
+        // assertion of name
+        Assert.assertEquals(text, getSongName());
+        // Find playlist
+        findCreatedPlaylist();
+
+        //Delete playlist
+        deleteCreatedPlaylist();
+
+        // Thread.sleep(3000);
+        isBannerDisplayed();
+
+
+
+
+    }
+
+    private void deleteCreatedPlaylist() {
+        WebElement deletePlaylist = driver.findElement(By.cssSelector("button[title='Delete this playlist']"));
+        deletePlaylist.click();
+        //  Thread.sleep(3000);
+        WebElement pressOK = driver.findElement(By.cssSelector("[tabindex='1']"));
+        pressOK.click();
+
+    }
+
+    private void findCreatedPlaylist() {
+        WebElement findPlaylist = driver.findElement(By.cssSelector("section#playlists > ul > li:nth-of-type(3) > a"));
+        findPlaylist.click();
+
+
+    }
+
+    private void inputSearch(String text) {
         WebElement searchInput = driver.findElement(By.cssSelector("[type='search']"));
         searchInput.click();
         searchInput.clear();
         searchInput.sendKeys(text);
-        // click results view all button
-        WebElement viewAllBtn = driver.findElement(By.xpath("//button[@data-test='view-all-songs-btn']"));
-        viewAllBtn.click();
-        // click first song
-        List<WebElement> songsInResults = driver.findElements(By.cssSelector(".search-results .song-item .title"));
-        songsInResults.get(0).click();
-        // click Add To
-        WebElement addToBtn = driver.findElement(By.cssSelector("[data-test='add-to-btn']"));
-        addToBtn.click();
-        // create new playlist
-        WebElement newPlaylistNameInput = driver.findElement(By.cssSelector("[id='songResultsWrapper'] [placeholder='Playlist name']"));
-        newPlaylistNameInput.click();
-        newPlaylistNameInput.clear();
-        newPlaylistNameInput.sendKeys(playlistName);
-        // click Enter
-        new Actions(driver)
-                .keyDown(Keys.ENTER)
-                .perform();
-        // assertions - success banner and song name in playlist
-        WebElement successBanner = driver.findElement(By.cssSelector(".success"));
-        Assert.assertTrue(successBanner.isDisplayed());
-        WebElement songName = driver.findElement(By.cssSelector("#playlistWrapper .song-item .title"));
-        String songText = songName.getText();
-        Assert.assertEquals(text, songText);
-        // Find playlist
-        WebElement findPlaylist = driver.findElement(By.cssSelector("section#playlists > ul > li:nth-of-type(3) > a"));
-        findPlaylist.click();
-        //Delete playlist
-        WebElement deletePlaylist = driver.findElement(By.cssSelector("button[title='Delete this playlist']"));
-        deletePlaylist.click();
-        Thread.sleep(3000);
-        WebElement pressOK = driver.findElement(By.cssSelector("[tabindex='1']"));
-        pressOK.click();
-        Thread.sleep(3000);
-        WebElement deleteSuccess = driver.findElement(By.cssSelector(".success"));
-        Assert.assertTrue(deleteSuccess.isDisplayed());
+
+
     }
 }
