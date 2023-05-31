30c8bc4643856c43eff4e9cf912e8d2da170cdc1
vikenty19 <vikenty.plakhov@testpro.io>
2023-05-31 14:19:43 -0400

    testNG

--- src/test/java/Homework21.java
+++ src/test/java/Homework21.java
@@ -8,23 +8,20 @@ import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-
-public class Homework20 extends BaseTest {
-
+public class Homework21 extends BaseTest{
     @Test
-    public void deletePlaylist()  {
+    public void renamePlaylist(){
+        String playlistName =  generateRandomPlaylistName();
         login("vicplach123@gmail.com", "MEGAdelta06@");
-        String playlistName = generateRandomPlaylistName();
 
-        //create playlist before deleting it
 
+        //create playlist
         WebElement plusPlaylistbtn = wait.until(ExpectedConditions
                 .visibilityOfElementLocated(By.cssSelector("[title='Create a new playlist']")));
         plusPlaylistbtn.click();
         WebElement newPlaylistbtn = wait.until(ExpectedConditions
                 .elementToBeClickable(By.cssSelector("[data-testid='playlist-context-menu-create-simple']")));
         newPlaylistbtn.click();
-
         WebElement inputNewPlaylist = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[name='name']")));
 
         inputNewPlaylist.click();
@@ -33,38 +30,22 @@ public class Homework20 extends BaseTest {
         new Actions(driver)
                 .keyDown(Keys.ENTER)
                 .perform();
-       // Thread.sleep(300);
-        // playlist appearance confirmation
+        System.out.println(playlistName);
+       //check playlist name
+
         WebElement newPlaylistConfirm = driver.findElement(By.cssSelector("section#playlistWrapper h1"));
 
         wait.until(ExpectedConditions.textToBePresentInElement(newPlaylistConfirm, playlistName));
         // check the playlist creation
         System.out.println(newPlaylistConfirm.getText());
         Assert.assertEquals(newPlaylistConfirm.getText(), playlistName);
-        //delete playlist
-        WebElement playlistDelete = wait.until(ExpectedConditions
-                .elementToBeClickable(By.cssSelector(".btn-delete-playlist")));
-        playlistDelete.click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".success")));
 
-        System.out.println("OK");
-        // refresh page
-        driver.navigate().refresh();
+        //rename playlist
 
-        //Thread.sleep(1000);
 
-        List<WebElement> PlayLists = driver.findElements(By.cssSelector("#playlists a"));
-        List<String> PlaylistNames = new ArrayList<>();
-        //playlist deleting check
 
-        for (int i = 0; i < PlayLists.size(); i++) {
-            String PlaylistName = PlayLists.get(i).getText();
-            PlaylistNames.add(PlaylistName);
-
-        }
-        System.out.println(PlaylistNames);
-        Assert.assertFalse(PlaylistNames.contains(playlistName));
     }
+
 
 
 }
