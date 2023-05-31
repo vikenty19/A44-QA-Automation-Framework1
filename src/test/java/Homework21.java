b29e8da077d60d05dcd9dcdce894ea26e3b109ba
vikenty19 <vikenty.plakhov@testpro.io>
2023-05-31 15:17:20 -0400

    Update Homework21.java

--- src/test/java/Homework21.java
+++ src/test/java/Homework21.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.SourceType;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -11,11 +12,35 @@ import java.util.List;
 public class Homework21 extends BaseTest{
     @Test
     public void renamePlaylist(){
-        String playlistName =  generateRandomPlaylistName();
+        String playlistName = "Stay tune" ;
+        String  newName = "Don't feel sorry";
+
         login("vicplach123@gmail.com", "MEGAdelta06@");
 
 
         //create playlist
+        createNewPlist(playlistName);
+
+       //check playlist name
+        checkTheNameOfCreatedPlyst(playlistName);
+
+        System.out.println(playlistName);
+
+
+        //rename playlist
+        doubleClickChoosePlaylist();
+        enterNewName(newName);
+        System.out.println(newName);
+        Assert.assertFalse(playlistName.contains(newName));
+
+
+
+
+
+
+    }
+
+    private void createNewPlist(String playlistName) {
         WebElement plusPlaylistbtn = wait.until(ExpectedConditions
                 .visibilityOfElementLocated(By.cssSelector("[title='Create a new playlist']")));
         plusPlaylistbtn.click();
@@ -30,22 +55,29 @@ public class Homework21 extends BaseTest{
         new Actions(driver)
                 .keyDown(Keys.ENTER)
                 .perform();
-        System.out.println(playlistName);
-       //check playlist name
-
-        WebElement newPlaylistConfirm = driver.findElement(By.cssSelector("section#playlistWrapper h1"));
-
-        wait.until(ExpectedConditions.textToBePresentInElement(newPlaylistConfirm, playlistName));
-        // check the playlist creation
-        System.out.println(newPlaylistConfirm.getText());
-        Assert.assertEquals(newPlaylistConfirm.getText(), playlistName);
-
-        //rename playlist
-
 
 
     }
 
+    private void checkTheNameOfCreatedPlyst(String playlistName) {
+        WebElement newPlaylistConfirm = driver.findElement(By.cssSelector("section#playlistWrapper h1"));
+
+        wait.until(ExpectedConditions.textToBePresentInElement(newPlaylistConfirm, playlistName));
+        Assert.assertEquals(newPlaylistConfirm.getText(), playlistName);
+    }
+
+    public void enterNewName(String newName) {
+        WebElement playlistInputField = driver.findElement(By.cssSelector("input[name='name']"));
+        playlistInputField.sendKeys(Keys.HOME, Keys.chord(Keys.SHIFT, Keys.END), newName);
+        playlistInputField.sendKeys(Keys.ENTER);
+    }
 
 
-}
+    private void doubleClickChoosePlaylist() {
+        WebElement playlistElement = wait.until(ExpectedConditions.
+                elementToBeClickable(By.cssSelector(".playlist:nth-child(3)")));
+        Actions actions = new Actions(driver);
+        actions.doubleClick(playlistElement).perform();
+    }
+    }
+
