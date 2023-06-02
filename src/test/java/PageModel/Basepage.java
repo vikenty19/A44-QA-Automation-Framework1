8bed810d76a306610be9e1f5ccac538ab8c4d4f7
vikenty19 <vikenty.plakhov@testpro.io>
2023-06-02 11:15:06 -0400

    Homework22

---
+++ src/test/java/PageModel/Basepage.java
@@ -0,0 +1,37 @@
+package PageModel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Basepage {
+
+    WebDriver driver;
+    WebDriverWait wait;
+    Actions actions;
+
+ public Basepage(WebDriver givenDriver) {
+
+     driver = givenDriver;
+     wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+     actions = new Actions(driver);
+ }
+     public WebElement waitUntilVisible(By element){
+         return new WebDriverWait(driver, Duration.ofSeconds(4)).until(ExpectedConditions.visibilityOfElementLocated(element));
+     }
+
+     public WebElement waitUntilClickable(By element){
+         return new WebDriverWait(driver, Duration.ofSeconds(4)).until(ExpectedConditions.elementToBeClickable(element));
+     }
+
+
+ }
+
+
+
+
---
+++ src/test/java/PageModel/LoginPage.java
@@ -0,0 +1,42 @@
+package PageModel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class LoginPage extends Basepage {
+    public LoginPage(WebDriver givenDriver) {
+        super(givenDriver);
+    }
+    By emailField = By.cssSelector("[type='email']");
+    By  passwordField = By.cssSelector("[type='password']");
+  By submitLoginBtn = By.cssSelector("button[type='submit']");
+
+
+    public void enterEmail(String email) {
+        WebElement emailInput = wait.until(ExpectedConditions.visibilityOfElementLocated(emailField));
+        emailInput.click();
+        emailInput.clear();
+        emailInput.sendKeys(email);
+    }
+
+
+    public void enterPassword(String password) {
+        WebElement passwordInput = wait.until(ExpectedConditions.elementToBeClickable(passwordField));
+        passwordInput.click();
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
+    }
+
+    public void clickLoginBtn() {
+        WebElement submitLogin = driver.findElement(submitLoginBtn);
+        submitLogin.click();
+    }
+
+    public void login(String email, String password) {
+        enterEmail(email);
+        enterPassword(password);
+        clickLoginBtn();
+    }
+}
