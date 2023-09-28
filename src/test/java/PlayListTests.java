b780e78a77023d398e52fd807feaca94a24092c0
vikenty19 <vikenty.plakhov@testpro.io>
2023-09-28 16:59:24 -0400

    Update PlayListTests.java

--- src/test/java/PlayListTests.java
+++ src/test/java/PlayListTests.java
@@ -16,7 +16,7 @@ public class PlayListTests extends BaseTest {
 
 
         login("demo@class.com", "te$t$tudent");
-//        WebElement plusBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".fa-plus-circle")));
+   //    WebElement plusBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".fa-plus-circle")));
 
         WebElement plusBtn = waitUntilVisible(By.cssSelector(".fa-plus-circle"));
 
@@ -33,18 +33,18 @@ public class PlayListTests extends BaseTest {
                 .elementToBeClickable(By.cssSelector("input[name='name']")));
         enterField.click();
         enterField.clear();
-    //    enterField.sendKeys(playlistName);
+//    enterField.sendKeys(playlistName);
 
-        new Actions(driver).keyDown(Keys.ENTER)
-                             .sendKeys(playlistName)
+        new Actions(driver).sendKeys(playlistName)
+                            .keyDown(Keys.ENTER)
                              .perform();
         Thread.sleep((5000));
         WebElement playListHeader = waitUntilVisible(By.cssSelector("#playlistWrapper h1"));
         Assert.assertEquals(playListHeader.getText(),playlistName);
         //delete playlist
- /*       WebElement deletePlistBtn = waitUntilClickable(By
+        WebElement deletePlistBtn = waitUntilClickable(By
                 .cssSelector(".btn-delete-playlist"));
-        deletePlistBtn.click();*/
+        deletePlistBtn.click();
 
     }
 }
