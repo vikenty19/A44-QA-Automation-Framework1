f2f53fed8a54b0456b97839b34def09824f23a43
vikenty19 <vikenty.plakhov@testpro.io>
2023-09-29 19:14:08 -0400

    Update PlaySongs.java

--- src/test/java/PlaySongs.java
+++ src/test/java/PlaySongs.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 public class PlaySongs extends BaseTest {
 
     @Test
-    public void playSong()  {
+    public void playSong() {
 
         login("vicplach123@gmail.com", "MEGAdelta06@");
         // all songs
@@ -22,16 +22,17 @@ public class PlaySongs extends BaseTest {
     }
 
     @Test
-    public void playSongOverPlayBtn() {
+    public void playSongOverPlayBtn() throws InterruptedException {
         login("vicplach123@gmail.com", "MEGAdelta06@");
         goToAllSongsTub();
         //choose the song from the allSongs list
-        WebElement selectedSong = waitUntilClickable(By.cssSelector("tr:nth-child(6) .title"));
+        WebElement selectedSong = waitUntilClickable(By.cssSelector("tr:nth-child(1) .title"));
         selectedSong.click();
         playSongWithPlayBtn();
+        Thread.sleep(3000);
         isEqualizerDisplayed();
-
     }
+
     private void isPauseBtnDisplayed() {
         WebElement pauseBtn = driver.findElement(By.cssSelector("span[role='button'] > .fa.fa-pause"));
         new Actions(driver)
@@ -39,14 +40,14 @@ public class PlaySongs extends BaseTest {
                 .perform();
         pauseBtn.click();
         Assert.assertTrue(pauseBtn.isDisplayed());
-        System.out.println("Is pause btn displayed?  "+ pauseBtn.isDisplayed());
+        System.out.println("Is pause btn displayed?  " + pauseBtn.isDisplayed());
     }
 
     private void isEqualizerDisplayed() {
         WebElement equalizer = wait.until(ExpectedConditions
                 .visibilityOfElementLocated(By.cssSelector(".bars")));
         Assert.assertTrue(equalizer.isDisplayed());
-        System.out.println("Is equalizer displyed  "+ equalizer.isDisplayed());
+        System.out.println("Is equalizer displayed  " + equalizer.isDisplayed());
     }
 
     private void playSongWithPlayBtn() {
