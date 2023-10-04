e951c387bd399bf636601097839a9558714a6f0d
vikenty19 <vikenty.plakhov@testpro.io>
2023-10-04 18:14:42 -0400

    Login page
    locators added

---
+++ src/test/java/POM/LoginPage.java
@@ -0,0 +1,40 @@
+package POM;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+
+public class LoginPage extends BasePage{
+
+    public LoginPage(WebDriver givenDriver) {
+        super(givenDriver);
+    }
+
+    By emailField = By.cssSelector("[type='email']");
+    By passwordField = By.cssSelector("[type='password']");
+    By loginBtn =By.cssSelector("button[type='submit']");
+    protected void clickLoginBtn() {
+        WebElement submitLogin = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(loginBtn));
+        submitLogin.click();
+    }
+
+    public void enterPassword(String password) {
+        WebElement passwordInput = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(passwordField));
+        passwordInput.click();
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
+    }
+
+    protected void enterEmail(String email) {
+        WebElement emailInput = wait.until(ExpectedConditions
+                .visibilityOfElementLocated( emailField  ));
+        emailInput.click();
+        emailInput.clear();
+        emailInput.sendKeys(email);
+    }
+
+}
