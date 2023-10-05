4dfca01ef4855ba2c7798843c6a3f9fbd0e07309
vikenty19 <vikenty.plakhov@testpro.io>
2023-10-05 18:35:07 -0400

    .

---
+++ src/test/java/POM/PlayListPage.java
@@ -0,0 +1,41 @@
+package POM;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class PlayListPage extends BasePage{
+    public PlayListPage(WebDriver givenDriver) {
+        super(givenDriver);
+    }
+
+    public void clearPlayLIstNameField() {
+        WebElement pListNameToDelete = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)>a")));
+        System.out.println(pListNameToDelete.getText());
+        new Actions(driver)
+                .doubleClick(pListNameToDelete)
+                .perform();
+
+    }
+    public void enterPlaylistName(String name) {
+        WebElement playlistInputField = waitUntilClickable(By.cssSelector("input[name='name']"));
+        playlistInputField.sendKeys(Keys.HOME, Keys.chord(Keys.SHIFT, Keys.END), name);
+        playlistInputField.sendKeys(Keys.ENTER);
+    }
+    public void plusBtnClick() {
+        WebElement plusBtn = waitUntilVisible(By.cssSelector(".fa-plus-circle"));
+        plusBtn.click();
+    }
+
+    public void goToPlayListField() {
+        WebElement newPlaylist = wait.until(ExpectedConditions
+                .elementToBeClickable(By.cssSelector("[data-testid='playlist-context-menu-create-simple']")));
+
+
+    }
+}
+
--- src/test/java/PlayListTests.java
+++ src/test/java/PlayListTests.java
@@ -1,4 +1,5 @@
 import POM.LoginPage;
+import POM.PlayListPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -18,16 +19,14 @@ public class PlayListTests extends BaseTest {
     public void deletePlaylistTest() throws InterruptedException {
         String playlistName = generateRandomPlaylistBookName();
         System.out.println(playlistName);
+        PlayListPage playListPage = new PlayListPage(driver);
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login("demo@class.com", "te$t$tudent");
-        WebElement plusBtn = waitUntilVisible(By.cssSelector(".fa-plus-circle"));
 
-        plusBtn.click();
+        playListPage.plusBtnClick();
+        playListPage.goToPlayListField();
 
-        WebElement newPlaylist = wait.until(ExpectedConditions
-                .elementToBeClickable(By.cssSelector("[data-testid='playlist-context-menu-create-simple']")));
-
-        newPlaylist.click();
+  //      newPlaylist.click();
 //
         //playlist name Enter
 
@@ -38,88 +37,51 @@ public class PlayListTests extends BaseTest {
 //    enterField.sendKeys(playlistName);
 
         new Actions(driver).sendKeys(playlistName)
-                            .keyDown(Keys.ENTER)
-                             .perform();
-  //       Thread.sleep(1000);
-         //Assertions of playlist name
-        WebElement playListHeader =driver.findElement(By.cssSelector("#playlistWrapper h1"));
-        wait.until(ExpectedConditions.textToBePresentInElement(playListHeader,playlistName));
-        Assert.assertEquals(playListHeader.getText(),playlistName);
+                .keyDown(Keys.ENTER)
+                .perform();
+        //       Thread.sleep(1000);
+        //Assertions of playlist name
+        WebElement playListHeader = driver.findElement(By.cssSelector("#playlistWrapper h1"));
+        wait.until(ExpectedConditions.textToBePresentInElement(playListHeader, playlistName));
+        Assert.assertEquals(playListHeader.getText(), playlistName);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".success")));
         //delete playlist
         WebElement deletePlistBtn = waitUntilClickable(By
                 .cssSelector(".btn-delete-playlist"));
         deletePlistBtn.click();
-       Thread.sleep(1000);
+        Thread.sleep(1000);
         driver.navigate().refresh();
-        List<WebElement> playlistTable =driver.findElements(By.cssSelector(".playlist.playlist>a"));
-        List<String >playListNames = new ArrayList<>();
-        for (int i = 2; i< playlistTable.size();i ++) {  // i=2 to not include favorites,recently tabs
-         String playlName = playlistTable.get(i).getText();
-         playListNames.add(playlName);
+        List<WebElement> playlistTable = driver.findElements(By.cssSelector(".playlist.playlist>a"));
+        List<String> playListNames = new ArrayList<>();
+        for (int i = 2; i < playlistTable.size(); i++) {  // i=2 to not include favorites,recently tabs
+            String playlName = playlistTable.get(i).getText();
+            playListNames.add(playlName);
 
-          Assert.assertNotEquals(playListNames.get(i-2),playlistName);//
+            Assert.assertNotEquals(playListNames.get(i - 2), playlistName);//
 
         }
         System.out.println(playListNames);
         Assert.assertFalse(playListNames.contains(playlistName));
     }
 
+
     @Test
-    public void renamePlayList() throws InterruptedException {
-        String playlistName = generateRandomPlaylistBookName();
+    public void renamePlayList() {
         String newPlayLIstName = "Mermaid";
-        System.out.println(playlistName);
-
-
+        PlayListPage playListPage = new PlayListPage(driver);
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login("demo@class.com", "te$t$tudent");
-        Thread.sleep((3000));
 
-
-     WebElement pListNameToDelete = wait.until(ExpectedConditions
-             .visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)>a")));
-
-        new Actions(driver)
-                .doubleClick(pListNameToDelete)
-                .perform();
-        Thread.sleep(3000);
-         enterPlaylistName(newPlayLIstName);
+        playListPage.clearPlayLIstNameField();
+        playListPage.enterPlaylistName(newPlayLIstName);
         System.out.println(newPlayLIstName);
-    /*    new Actions(driver)
-                .keyDown(Keys.ENTER)
-                        .perform();*/
-
-
- /*       new Actions(driver)
-                .contextClick(pListNameToDelete)
-                .perform();
-        Thread.sleep((3000));
-        driver.navigate().refresh();
-        WebElement edit = driver.findElement(By
-                .cssSelector(" .playlist-item-menu [data-testid='playlist-context-menu-edit-71974']"));
-        new Actions(driver)
-                .click(edit)
-                .perform();
-        Thread.sleep(3000);
-        enterPlaylistName(newPlayLIstName);
-   pListNameToDelete.click();
-   //    pListNameToDelete.clear();
-        new Actions(driver)
-                .sendKeys("Mermaid")
-                .perform();*/
-    //    Thread.sleep(3000);
-
         System.out.println(getPlaylistName());
-     Assert.assertEquals(newPlayLIstName,getPlaylistName());
+        Assert.assertEquals(newPlayLIstName, getPlaylistName());
     }
-    public void enterPlaylistName(String name) {
-        WebElement playlistInputField = driver.findElement(By.cssSelector("input[name='name']"));
-        playlistInputField.sendKeys(Keys.HOME, Keys.chord(Keys.SHIFT, Keys.END), name);
-        playlistInputField.sendKeys(Keys.ENTER);
-    }
-    public String getPlaylistName(){
-        WebElement playlistInputField =wait.until(ExpectedConditions
+
+
+    public String getPlaylistName() {
+        WebElement playlistInputField = wait.until(ExpectedConditions
                 .visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)>a")));
         String name = playlistInputField.getText();
         return name;
