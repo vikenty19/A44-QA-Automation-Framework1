3262b8a1a8d95cf583afc21416f9cf795e1807ea
vikenty19 <vikenty.plakhov@testpro.io>
2023-10-13 19:25:59 -0400

    Update ProfileTests.java

--- src/test/java/ProfileTests.java
+++ src/test/java/ProfileTests.java
@@ -5,8 +5,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.interactions.WheelInput;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProfileTests extends BaseTest {
 
@@ -46,11 +50,35 @@ public class ProfileTests extends BaseTest {
         profilePage.openProfile();
         Thread.sleep(3000);
      //   WebElement colorPane = driver.findElement(By.tagName("li:nth-of-type(1) > .theme > .name"));
-        WebElement footer = driver.findElement(By.cssSelector("li:nth-of-type(8) > .theme > .name"));
+
+
+        List<WebElement> backgrounds =driver.findElements(By.cssSelector(".theme .name"));
+        List<String> backColors = new ArrayList<>();
+        for (int i = 0; i < backgrounds.size(); i++) {
+          //  backgrounds.get(i).click();
+
+            new Actions(driver)
+                    .moveToElement(backgrounds.get(i))
+                    .perform();
+            String color = backgrounds.get(i).getText();
+            Thread.sleep(1000);
+            backColors.add(color);
+        }
+
+            System.out.println(backColors+" "+ backColors.size());
+
+
+      //  System.out.println(backgrounds.get(1).getText());
+      //  System.out.println(backgrounds);
+
+
+
+        WebElement footer = driver.findElement(By.cssSelector("li:nth-of-type(1) > .theme > .name"));
         WheelInput.ScrollOrigin scrollOrigin = WheelInput.ScrollOrigin.fromElement(footer, 0, -50);
         new Actions(driver)
                 .scrollFromOrigin(scrollOrigin,0, 200)
                 .perform();
+        System.out.println(footer.getText());
         Thread.sleep(5000);
     }
 }
