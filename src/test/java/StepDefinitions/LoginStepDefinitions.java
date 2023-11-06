fa407d9de0c274a402041fa4c648ed8d92315912
vikenty19 <vikenty.plakhov@testpro.io>
2023-11-06 15:49:48 -0500

    Update LoginStepDefinitions.java

--- src/test/java/StepDefinitions/LoginStepDefinitions.java
+++ src/test/java/StepDefinitions/LoginStepDefinitions.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
 
@@ -64,6 +65,6 @@ public class LoginStepDefinitions {
     public void iLoggedIn(){
 
         HomePage homePage = new HomePage(driver);
-        homePage.getAvatar();
+       Assert.assertTrue(homePage.getAvatar());
     }
 }
