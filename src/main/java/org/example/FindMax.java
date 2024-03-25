4c2710ab9da603faff950fada449781ffe9e6fc6
vikenty19 <vikenty.plakhov@testpro.io>
2024-03-25 16:11:37 -0400

    Update FindMax.java

--- src/main/java/org/example/FindMax.java
+++ src/main/java/org/example/FindMax.java
@@ -2,14 +2,17 @@ package org.example;
 
 public class FindMax {
     public static void main(String[] args) {
-        var array = new int[]{34, 55, 67, 23, 0};
+       var array = new int[]{34, 55, 67, 23, 0};
+       int[] array1 = null;
         System.out.println(max(array) + " 67");
         System.out.println(max(new int[0]) + "  null");
         System.out.println(max(new int[]{88}) + "  88");
+        System.out.println(max(array1)+ "  null");
+        System.out.println(max(null) + " another null");
     }
 
     public static Integer max(int[] arr) {
-        if (arr.length == 0) {
+        if (arr==null||arr.length == 0) {
             return null;
         }
         int maximum = arr[0];
