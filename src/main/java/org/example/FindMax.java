3abe23457f9314e6ec3da61714076a2f8317ad23
vikenty19 <vikenty.plakhov@testpro.io>
2024-03-25 16:55:52 -0400

    search index with Optional

--- src/main/java/org/example/FindMax.java
+++ src/main/java/org/example/FindMax.java
@@ -1,14 +1,21 @@
 package org.example;
 
+import java.util.Optional;
+
 public class FindMax {
     public static void main(String[] args) {
        var array = new int[]{34, 55, 67, 23, 0};
        int[] array1 = null;
-        System.out.println(max(array) + " 67");
-        System.out.println(max(new int[0]) + "  null");
-        System.out.println(max(new int[]{88}) + "  88");
-        System.out.println(max(array1)+ "  null");
-        System.out.println(max(null) + " another null");
+//        System.out.println(max(array) + " 67");
+//        System.out.println(max(new int[0]) + "  null");
+//        System.out.println(max(new int[]{88}) + "  88");
+//        System.out.println(max(array1)+ "  null");
+//        System.out.println(max(null) + " another null");
+        System.out.println(indexFind(array,23) + "3");
+        System.out.println(indexFind(array,0) + "4");
+        System.out.println(indexFind(array,77) + "  empty");
+        System.out.println(indexFind(null,77) + "  empty");
+
     }
 
     public static Integer max(int[] arr) {
@@ -22,5 +29,16 @@ public class FindMax {
             }
         }
         return maximum;
+    }
+    public static Optional<Integer> indexFind(int[]arr,int querry) {
+           if(arr==null||arr.length==0){
+               return Optional.empty();
+           }
+        for (int i = 0; i <arr.length ; i++) {
+            if(querry==arr[i]){
+                return Optional.of(i);
+            }
+        }
+        return Optional.empty();
     }
 }
