0789a80540a97d8699acec81f706e5c04a833833
vikenty19 <vikenty.plakhov@testpro.io>
2024-03-26 12:26:11 -0400

    palindrome added

--- src/main/java/org/example/Task1299.java
+++ src/main/java/org/example/Task1299.java
@@ -5,17 +5,20 @@ import java.util.List;
 
 public class Task1299 {
     public static void main(String[] args) {
-     Task1299 task = new Task1299();
-     int[]example = {17,18,5,4,6,1};
+        Task1299 task = new Task1299();
+        int[] example = {17, 18, 5, 4, 6, 1};
+        String examp = "KoLeso";
         System.out.println(Arrays.toString(task.replaceElement(new int[]{400})));
         System.out.println(Arrays.toString(task.replaceElement(example)));
+        System.out.println(isPalindrome(examp));
 
     }
-    public int[] replaceElement(int[]arr){
+
+    public int[] replaceElement(int[] arr) {
         int max = -1;
-        for (int i = arr.length-1; i >=0 ; i--) {
-            if(arr[i]>max){
-                max =arr[i];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] > max) {
+                max = arr[i];
             }
 
         }
@@ -23,4 +26,25 @@ public class Task1299 {
         return arr;
 
     }
+
+    public static boolean isPalindrome(String str) {
+        String rev = "";
+
+        // Initializing a new boolean variable for the
+        // answer
+        boolean ans = false;
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        // Checking if both the strings are equal
+        if (str.equals(rev)) {
+            ans = true;
+        }
+        return ans;
+
+
+    }
 }
+
