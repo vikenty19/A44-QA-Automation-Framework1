ff1e107bbae84a484d8b086973cb5247790bb17a
vikenty19 <vikenty.plakhov@testpro.io>
2024-03-28 12:42:37 -0400

    secondMinMax added

--- src/main/java/org/example/FindMaxAndNumberOfElement.java
+++ src/main/java/org/example/FindMaxAndNumberOfElement.java
@@ -4,18 +4,18 @@ import java.util.Optional;
 
 public class FindMaxAndNumberOfElement {
     public static void main(String[] args) {
-       var array = new int[]{34, 55, 67, 23, 0};
+       var array = new int[]{34,34,34};
        int[] array1 = null;
-        System.out.println(max(array) + " 67");
-        System.out.println(min(array) + "  0");
-//        System.out.println(max(new int[0]) + "  null");
+//        System.out.println(max(array) + " 67");
+ //       System.out.println(min(array) + "  0");
+        System.out.println(secondMin(array) + "  0");
 //        System.out.println(max(new int[]{88}) + "  88");
 //        System.out.println(max(array1)+ "  null");
 //        System.out.println(max(null) + " another null");
-        System.out.println(firstIndexFind(array,23) + "3");
-        System.out.println(firstIndexFind(array,0) + "4");
-        System.out.println(firstIndexFind(array,77) + "  empty");
-        System.out.println(firstIndexFind(null,77) + "  empty");
+//        System.out.println(firstIndexFind(array,23) + "3");
+//        System.out.println(firstIndexFind(array,0) + "4");
+//        System.out.println(firstIndexFind(array,77) + "  empty");
+//        System.out.println(firstIndexFind(null,77) + "  empty");
 
     }
 
@@ -54,5 +54,22 @@ public class FindMaxAndNumberOfElement {
             }
         }
         return Optional.empty();
+    }
+    public static Integer secondMin(int[]arr){
+        if (arr==null||arr.length <2 ) {
+            return null;
+        }
+        int min = arr[0];
+        int min2 = arr[1];
+        for (int i = 1; i <arr.length ; i++) {
+            if(arr[i]<min){
+                min2=min;
+                min = arr[i];
+
+            } else if (arr[i]<min2 && arr[i]!= min2 ){
+                min2 = arr[i];
+            }
+
+        } return min2;
     }
 }
